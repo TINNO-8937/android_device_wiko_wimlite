1fa0dd62ebc96cca9d2fc02ba3f7e6703ec833ee
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    wimlite: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: I0fbb03603a72d0108a96bfa0f0a4ceb12ef3d430

--- device.mk
+++ device.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/etc/media_profiles_V1_0.xml
 
 # NFC
 PRODUCT_COPY_FILES += \
