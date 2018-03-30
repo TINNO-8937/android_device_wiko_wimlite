d7bb8d90ce8e4e247fc1c5559b0d4a3fbe3c8bfd
Ruchi Kandoi <kandoiruchi@google.com>
2018-03-30 15:24:37 -0700

    wimlite: Add support for NFC 1.1
    Test: Enable/Disable NFC, Tag reading
    Bug: 75980364
    Change-Id: I337810ff89d61f796cb213cd931a7b665870029e

--- device.mk
+++ device.mk
@@ -69,8 +69,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-mtp_rf1.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-mtp_rf1.conf \
-    $(LOCAL_PATH)/nfc/libnfc-mtp_rf2.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-mtp_rf2.conf
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
 
 # Sensors
 PRODUCT_COPY_FILES += \
