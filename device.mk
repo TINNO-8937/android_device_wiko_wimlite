7dbe2c17c0d84500d98be14fb99ac7ba7eccf320
Louis Popi <theh2o64@gmail.com>
2018-07-14 21:06:03 +0200

    wimlite: Use custom overlay

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/wiko/wimlite/wimlite-vendor.mk)
 
+# Local overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Inherit from msm8996-common
 $(call inherit-product, device/tinno/msm8937-common/msm8937.mk)
 
