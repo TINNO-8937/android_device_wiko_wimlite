86a59fc07bb899bd2bb42b7eb094999358837a09
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:20:32 +0430

    wimlite: Drop module eng tags
    Change-Id: I2e84d598b2412e617e5575bfdefd804612f76889
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
