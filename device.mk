9a7cc12e2afd772d5f662492a0b7bca39d17640f
Louis Popi <theh2o64@gmail.com>
2019-11-04 08:09:44 +0100

    Revert "wimlite: Copy camera config as msm8937_camera_c800"
    This reverts commit 15f8ee31d8edf086e6c296c1a20e54f04c94455e.

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/msm8937_camera_p6901.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8937_camera_c800.xml \
+    $(LOCAL_PATH)/configs/camera/msm8937_camera_p6901.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8937_camera_p7201.xml \
     $(LOCAL_PATH)/configs/camera/p6901_sunny_s5k3p3st_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/p6901_sunny_s5k3p3st_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/imx258_sunny_p6901_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx258_sunny_p6901_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/s5k3l8_sunny_p6901_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3l8_sunny_p6901_chromatix.xml \
